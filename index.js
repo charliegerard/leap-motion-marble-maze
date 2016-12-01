e06d39f1c2425afcf681c3af7d844bef20ceac6e
Charlie gerard <annecharlotte.gerard@gmail.com>
2016-12-01 22:20:07 +1100

    test

--- index.js
+++ index.js
@@ -14,29 +14,53 @@ board.on("ready", function(){
 		center: true
 	});
 
-	var controller = Leap.loop(function(frame){
-		if(frame.hands[0]){
-			var hand = frame.hands[0]
 
-			var previousFrame = controller.frame(1);
-			var axis = hand.rotationAxis(previousFrame);
+		myServo.sweep({
+		  range: [80, 85],
+		  interval: 1000,
+		  step: 10
+		});
+
+		myServo2.sweep({
+			range: [80, 85],
+			interval: 1000,
+			step: 10
+		});
+	//
+	// function servoOne(){
+	// 	myServo.to(90);
+	// 	setTimeout(function(){
+	// 		myServo.to(80)
+	// 	}, 1000)
+	// 	servoOne();
+	// }
+	// servoOne();
+
+	// var controller = Leap.loop(function(frame){
+		// if(frame.hands[0]){
+			// var hand = frame.hands[0]
+
+			// var previousFrame = controller.frame(1);
+			// var axis = hand.rotationAxis(previousFrame);
 			// console.log("Axis of Rotation: (" + axis[2] + "");
-			if(axis[0] < -0.5){
-				console.log('hand backwards')
-				myServo.to(70);
-			} else if(axis[0] > 0.7){
-				console.log('hand forward')
-				myServo.to(90);
-			}
+			// if(axis[0] < -0.5){
+				// console.log('hand backwards')
+				myServo.to(80);
+				// myServo.sweep([70,90]);
+			// } else if(axis[0] > 0.7){
+				// console.log('hand forward')
+				// myServo.to(90);
+			// }
 
-			if(axis[2] > 0.9){
-				console.log('orientation right')
-				myServo2.to(70)
-			} else if (axis[2] < -0.9){
-				console.log('orientation left')
-				myServo2.to(90)
-			}
-		}
+			// if(axis[2] > 0.9){
+				// console.log('orientation right')
+				// myServo2.sweep([70, 90])
+				// myServo2.to(70)
+			// } else if (axis[2] < -0.9){
+				// console.log('orientation left')
+				// myServo2.to(90)
+			// }
+		// }
 
-	})
+	// })
 })
